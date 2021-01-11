225711d85c6462d67d0ad90b7434719c5dc58412
Daniel J. Bergmann <Danieljensbergmann@outlook.de>
2021-01-11 09:59:02 +0100

    my first subscriber

---
+++ smartphone.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+import rospy
+from std_msgs.msg import String
+
+def callback_receive_radio_data(msg):
+    rospy.loginfo("Message received: ")
+    rospy.loginfo(msg)
+
+    
+
+if __name__== '__main__':
+    rospy.init_node('smartphone')
+
+    sub = rospy.Subscriber("/robot_news_radio",callback_receive_radio_data)
+
+    #So eine art loop funktion, die die obrigen Zeilen immer wieder aufruft
+    rospy.spin()
